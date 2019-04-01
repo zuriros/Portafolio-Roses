3e18c0f8dbacbe894dfa28490cbb1aca31958a92
zuriros <hanami33rosas@gmail.com>
2019-04-01 14:56:04 -0500

    actualizando cambios

---
+++ app.js
@@ -0,0 +1,10 @@
+const event1 = document.getElementById("firsEvent");
+const event2 = document.getElementById("secondEvent");
+const event3 = document.getElementById("thirdEvent");
+const event4 = document.getElementById("fourthEvent");
+
+
+event1.addEventListener("click", () => window.open("https://zuriros.github.io/lim-2018-11-bc-core-am-cipher/src", "_blank"));
+event2.addEventListener("click", () => window.open("https://zuriros.github.io/lim-2018-11-bc-core-am-data-lovers/src", "_blank"));
+event3.addEventListener("click", () => window.open("https://zuriros.github.io/LIM008-social-network/src/", "_blank"));
+event4.addEventListener("click", () => window.open("https://github.com/zuriros/md-links", "_blank"));
