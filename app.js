1d1d9990ae99573baf89f85d1128e1a267fbb207
zuriros <hanami33rosas@gmail.com>
2019-11-11 21:14:53 -0500

    agregando proyectos a mi portafolio

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const event1 = document.getElementById("firsEvent");
 const event2 = document.getElementById("secondEvent");
 const event3 = document.getElementById("thirdEvent");
 const event4 = document.getElementById("fourthEvent");
+const event5 = document.getElementById("fifthEvent");
+const event6 = document.getElementById("sixthEvent");
 
 
 event1.addEventListener("click", () => window.open("https://zuriros.github.io/lim-2018-11-bc-core-am-cipher/src", "_blank"));
 event2.addEventListener("click", () => window.open("https://zuriros.github.io/lim-2018-11-bc-core-am-data-lovers/src", "_blank"));
 event3.addEventListener("click", () => window.open("https://zuriros.github.io/LIM008-social-network/src/", "_blank"));
 event4.addEventListener("click", () => window.open("https://github.com/zuriros/md-links", "_blank"));
+event5.addEventListener("click", () => window.open("https://zuriros.github.io/freelancer", "_blank"));
+event6.addEventListener("click", () => window.open("https://zuriros.github.io/LIM008-fe-burger-queen/src/", "_blank"));
